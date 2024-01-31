ab9e68099b25915e4b399f3d7494e5cc707ca522
Joona Niemenmaa <joonaoskari@hotmail.com>
2024-01-31 16:54:20 +0200

    jei

--- src/main/java/main/University.java
+++ src/main/java/main/University.java
@@ -1,13 +1,10 @@
 package main;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.util.ArrayList;
 
 public class University {
