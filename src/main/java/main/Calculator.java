f63fa34d2b6a2103062ace4f46d221068ea1adb9
Joona Niemenmaa <joonaoskari@hotmail.com>
2024-01-31 15:51:24 +0200

    LETS FGO

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -21,7 +21,8 @@ public class Calculator {
         return average;
     }
     public double getMedianGrade(Student student) {
-        if (student.getStudentGrades().size() == 0) {
+        int input_size = student.getStudentGrades().size();
+        if (input_size == 0) {
             return 0;
         }
         int[] grade_array = new int[student.getStudentGrades().size()];
@@ -31,8 +32,15 @@ public class Calculator {
             index++;
         }
         Arrays.sort(grade_array);
-        int median_index = (int)(Math.floor(grade_array.length / 2));
-        double median = grade_array[median_index];
+        int median_index = 0;
+        double median = 0;
+        int input_remainder = input_size % 2;
+        if (input_remainder == 1) {
+            median_index = (int)(Math.floor((double)(grade_array.length) / 2d));
+            median = grade_array[median_index];
+        } else if (input_remainder == 0) {
+            median = (grade_array[grade_array.length / 2] + grade_array[(grade_array.length / 2) + 1]) / 2;
+        }
         return median;
     }
 }
